d988c947468c9d8f3b64401bdc6a0b5144969406
Natascha Gamboa <na7as5cha010@gmail.com>
2018-05-17 09:58:53 -0400

    tests case #(2) Verifica que exista el metodo registrarUsuario.

--- test_cases.py
+++ test_cases.py
@@ -6,9 +6,14 @@ class seguridadTestCase(unittest.TestCase):
     def setUp(self):
         self.seguridad = Seguridad()
 
-    #Verifica que exista la clase self.seguridad.
+    #(1) Verifica que exista la clase self.seguridad.
     def test_seguridad(self):
         valid = self.seguridad
+        self.assertNotEqual(valid, None)
+
+    #(2) Verifica que exista el metodo registrarUsuario.
+    def test_registrar(self):
+        valid = self.seguridad.registrarUsuario("angel@gmail.com", "X7SJus62h7AHv", "X7SJus62h7AHv")
         self.assertNotEqual(valid, None)
 
 
