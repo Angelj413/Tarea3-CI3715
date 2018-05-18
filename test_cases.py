7b776eb0b4dbb84e14fcf0dcf7fe600c53b08490
Angelj413 <morante413@gmail.com>
2018-05-18 10:30:06 -0400

    test case #3:valida que el correo sea segun el estandar RFC822

--- test_cases.py
+++ test_cases.py
@@ -11,7 +11,10 @@ class seguridadTestCase(unittest.TestCase):
         valid = self.seguridad
         self.assertNotEqual(valid, None)
 
-
+    #Verifica que el id de correo sea valido segun el estandar RFC 822.
+    def test_EmailValid(self):
+        valid = self.seguridad.registrarUsuario("angel?@gmail.com", "X7SJus62h7AHv", "X7SJus62h7AHv")
+        self.assertEqual(valid, False)
  
 
 if __name__ == '__main__':
